20d34742230dc8abca0684bdd2527cdb52c4291f
Vasilis Koukoutis <vkoukoutis@hotmail.com>
2018-10-04 20:27:09 +0300

    - Important file for development mode in docker.

---
+++ src/config/dev.js
@@ -0,0 +1,7 @@
+export const config = {
+    mongoURI: 'mongodb://mongodb:27017/movieholic',
+    redisURI: 'redis://redis:6379',
+    cookieKey: 'xoaao20030302kkxkakxakxkakaa',
+    jwtKey: 'mv2018!!',
+    omdbApi: 'b6122932'
+};
