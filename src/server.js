8c88cf7182301a730de2c81bde6ed7d883aeab44
vkoukoutis <vkoukoutis@mni.gr>
2018-10-30 09:56:36 +0200

    - Fixed non useful cors function.

--- src/server.js
+++ src/server.js
@@ -18,17 +18,7 @@ mongoose.connect(config.mongoURI, { useNewUrlParser: true, useCreateIndex: true 
 
 const app = express();
 
-const whitelist = ['http://localhost:3000', 'http://movieholic.herokuapp.com', 'https://movieholic.herokuapp.com'];
-const corsOptions = {
-    origin: function (origin, callback) {
-        if (whitelist.indexOf(origin) !== -1 || !origin) {
-            callback(null, true);
-        } else {
-            callback(new Error('Not allowed by CORS'));
-        }
-    }
-};
-app.use(cors(corsOptions.origin()));
+app.use(cors();
 app.use(bodyParser.json());
 app.use('/api', [authRouter, movieRouter, rentalRouter, serieRouter]);
 
