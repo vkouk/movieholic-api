a708dbf0fd9a32059e68234a19ce22ef0bb1e6dc
vkoukoutis <vkoukoutis@mni.gr>
2018-11-01 10:41:48 +0200

    - BETA phase of rent controller & post request. Implementations will be done in further commits.

--- src/controllers/RentalController.js
+++ src/controllers/RentalController.js
@@ -1,29 +1,63 @@
 import { Rental } from '../models/Rental';
+import { Movie } from '../models/Movie';
+import { Serie } from '../models/Serie';
+import { User } from '../models/User';
+import { getEntryByTitleParam } from './QueryController';
 import _ from 'lodash';
 
 const calculateAmount = data => {
-    if (data.movie && !data.serie) {
-        data.returnFee(_.toNumber(data.movie.rating));
-    } else if (data.serie && !data.movie) {
-        data.returnFee(_.toNumber(data.serie.rating));
-    } else {
-        data.map(record => {
-            if (record.movie && !record.serie) {
-                record.returnFee(_.toNumber(record.movie.rating));
-            } else if (record.serie && !record.movie) {
-                record.returnFee(_.toNumber(record.serie.rating));
-            } else {
-                record.returnFee(_.toNumber(record.movie.rating) + _.toNumber(record.serie.rating));
-            }
-        })
+    return data.map(record => {
+        if (record.movie && !record.serie) {
+            record.returnFee(_.toNumber(record.movie.rating));
+        } else if (record.serie && !record.movie) {
+            record.returnFee(_.toNumber(record.serie.rating));
+        } else {
+            record.returnFee(_.toNumber(record.movie.rating) + _.toNumber(record.serie.rating));
+        }
+    });
+};
+
+export const storeRent = userId => async (req, res) => {
+    const user = await User.findById(userId);
+    const movie = getEntryByTitleParam(Movie);
+    const serie = getEntryByTitleParam(Serie);
+
+    if (!user || (!movie && !serie)) {
+        return res.status(404).send('Seems something is missing');
     }
+
+    if (user.balance <= 0) {
+        return res.status(403).send("Sorry seems your balance isn't enough");
+    }
+
+    if (movie.stock <= 0 || serie.stock <= 0) {
+        return res.status(403).send('Oups, seems we have run out of stock of this product');
+    }
+
+    const rentalData = {
+        customer: user._id,
+        movie: movie._id,
+        serie: serie._id
+    };
+
+    return new Rental(rentalData).save().then(rent => {
+        const movie = rent.populate('movie');
+        const serie = rent.populate('serie');
+
+        if (movie && serie) {
+            movie.stock--;
+            serie.stock--;
+        } else if (serie) {
+            serie.stock--;
+        } else {
+            serie.stock--;
+        }
+
+        res.json(rent);
+    });
 };
 
-export const storeRent = (req, res) => {
-
-};
-
-export const getAllRents = (req, res, next) => {
+export const getAllRents = (req, res) => {
     return Rental.find({}).populate('customer').populate('movie').populate('serie')
         .exec((err, data) => {
             if (err) throw err;
