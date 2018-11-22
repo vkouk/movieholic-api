df69510822a24981b0286c4b2d3ce8af032b052b
vkoukoutis <vkoukoutis@mni.gr>
2018-11-22 10:13:21 +0200

    - Fixed hot issue with rental fee.

--- src/models/Rental.js
+++ src/models/Rental.js
@@ -30,11 +30,11 @@ const rentalSchema = new Schema({
 rentalSchema.methods.returnRental = function (movies, series) {
     this.dateReturned = new Date();
 
-    const moviesRating = movies.reduce((acc, curr) => acc + parseFloat(curr.rating), 0);
-    const seriesRating = series.reduce((acc, curr) => acc + parseFloat(curr.rating), 0);
+    const moviesRating = movies.reduce((acc, curr) => (typeof acc !== 'undefined' ? acc : 0) + (curr.rating !== 'N/A' ? parseFloat(curr.rating) : 1), 0);
+    const seriesRating = series.reduce((acc, curr) => (typeof acc !== 'undefined' ? acc : 0) + (curr.rating !== 'N/A' ? parseFloat(curr.rating) : 1), 0);
     const rentalRating = parseFloat(((moviesRating + seriesRating) / 10).toFixed(2));
-    const rentalDays = moment().diff(this.dateOrdered, 'days');
-    
+    const rentalDays = moment().diff(this.dateOrdered, 'days') + 1;
+
     this.rentalFee = (rentalDays * rentalRating * 0.9).toFixed(2);
 }
 
