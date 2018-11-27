72201a30dc589c792c30e340859d4535503e0a46
vkoukoutis <vkoukoutis@mni.gr>
2018-11-27 16:08:07 +0200

    - Most rented movies/series to show.

--- src/controllers/DashboardController.js
+++ src/controllers/DashboardController.js
@@ -10,3 +10,18 @@ export const getLatestMembers = async (req, res) => {
     const latestMembers = await User.find().sort('-joinedAt');
     res.json(latestMembers);
 };
+
+const countMostRentedData = array_of_titles => {
+    return array_of_titles.reduce((countWords, word) => {
+        countWords[word] = ++countWords[word] || 1;
+        return countWords;
+    }, {});
+};
+
+export const mostRented = async (req, res) => {
+    const allOrders = await Rental.find().sort('-dateOrdered').populate('movies').populate('series');
+    const allOrderMovies = countMostRentedData(allOrders.movies);
+    const allOrderSeries = countMostRentedData(allOrders.series);
+
+    res.json({ allOrderMovies, allOrderSeries });
+};
--- src/routes/dashboardRoutes.js
+++ src/routes/dashboardRoutes.js
@@ -1,8 +1,9 @@
 import express from 'express';
-import { getLatestOrders, getLatestMembers } from '../controllers/DashboardController';
+import { getLatestOrders, getLatestMembers, mostRented } from '../controllers/DashboardController';
 import requireAdmin from '../middlewares/requireAdmin';
 
 export const dashboardRouter = express.Router();
 
 dashboardRouter.get('/dashboard/orders', requireAdmin, getLatestOrders);
 dashboardRouter.get('/dashboard/members', requireAdmin, getLatestMembers);
+dashboardRouter.get('/dashboard/most-rented', requireAdmin, mostRented);
